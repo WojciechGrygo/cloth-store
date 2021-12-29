bd25b60290ca08a8a705ad538603382c36fd40a2
WojciechGrygoHTD <wojciech.grygo@htdevelopers.com>
2021-12-29 13:54:24 +0100

    New faker generator

--- cypress/support/faker.js
+++ cypress/support/faker.js
@@ -1,7 +1,37 @@
-import {  } from "faker";
+import { random, name, phone, address } from "faker";
 
-const method = () => {
-    return xxx;
+const randomNumber = () => {
+    return random.number;
 }
 
-export {};
+const randomName = () => {
+    return name.firstName;
+}
+
+const randomLastName = () => {
+    return name.lastName;
+}
+
+const randomPhoneNumber = () => {
+    return phone.phoneNumber;
+}
+
+const randomZipCode = () => {
+    return address.zipCode;
+}
+
+const randomCity = () => {
+    return address.city;
+}
+
+const randomAddress = () => {
+    return address.streetAddress;
+}
+
+const randomState = () => {
+    return address.state;
+}
+
+
+
+export { randomNumber, randomName, randomLastName, randomPhoneNumber, randomZipCode, randomCity, randomAddress, randomState };
